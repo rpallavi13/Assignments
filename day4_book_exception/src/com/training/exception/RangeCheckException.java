3e9b523d2f0f37f2d3976a70be3529dadf84293b
Pallavi Rani <42387441+rpallavi13@users.noreply.github.com>
2020-09-18 15:39:38 +0530

    Book Exception

---
+++ day4_book_exception/src/com/training/exception/RangeCheckException.java
@@ -0,0 +1,19 @@
+package com.training.exception;
+
+public class RangeCheckException extends Exception {
+
+	private String message;
+
+	public RangeCheckException(String message) {
+		super();
+		this.message=message;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "Exception"+message;
+	}
+
+}
